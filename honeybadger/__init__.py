47373f52db13428981d74b12c81de68fde646688
DMazzei <danielmazzei@gmail.com>
2017-11-01 10:47:36 +0000

    Removing the sys.excephook wrapper in initialization

--- honeybadger/__init__.py
+++ honeybadger/__init__.py
@@ -7,11 +7,6 @@ Use cases:
 >>> honeybadger.context(**kwargs)
 """
 
-import sys
-from .core import Honeybadger
 from .version import __version__
 
 __all__ = ['honeybadger', '__version__']
-
-honeybadger = Honeybadger()
-honeybadger.wrap_excepthook(sys.excepthook)
